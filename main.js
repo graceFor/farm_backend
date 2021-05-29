ba00b5618d782b8b1d1d4e1cca13cae9e7ae14bb
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-05-29 09:15:23 +0900

    Setting

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 var http = require("http");
 var url = require("url");
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  console.log(queryData);
+  var pathname = url.parse(_url, true).pathname;
+  console.log(pathname);
+  if (pathname === "/") {
+    // main home
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
+});
+
+app.listen(3000);
